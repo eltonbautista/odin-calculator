f0fcbab50fbbd17543995db7115fe78e1b375b41
Elton Bautista <eltonbestaha@gmail.com>
2022-01-17 14:36:43 -0700

    So I have the simple operators working, but you need to hit the equals sign which invokes the operate function. I can't get it working chained yet.

--- myscript.js
+++ myscript.js
@@ -3,12 +3,13 @@
 // Notice the parens at the end of the squiggly operations() squiggly bracket. 
 // This automatically adds the () so that it can be called such as: operations.add(a, b) rather than operations().add(a,b)
 const display = document.querySelector('#display');
-const btnContainer = document.querySelector('#button-container');
+const numberBtns = document.querySelectorAll('.number-button');
+const operatorBtns = document.querySelectorAll('.math-operator');
 display.innerText = '';
-//const ans = 0;
-let result = [0];
-let firstArg = '';
-let secondArg;
+let storedOperator;
+let result;
+let firstArg = 0;
+let secondArg = 0;
 
 const operations = function() {
     const calculator = {}
@@ -20,7 +21,7 @@ const operations = function() {
         return a - b;
     }
     calculator.multiply = function(a, b) {
-        if (a === 0) {
+        if (b === 0) {
             return 1*b
         }; 
         return (a*b);
@@ -32,83 +33,70 @@ const operations = function() {
     return calculator;
 }(); 
 
-const numbers = function() {
-    const numberButtons = {}
-
-    numberButtons.one = function() {
-        display.innerText += 1;
-        return firstArg += 1;
-    }
-    numberButtons.two = function() {
-         display.innerText += 2;
-         return firstArg += 2
-    }
-    numberButtons.three = function() {
-        display.innerText += 3;
-        return firstArg += 3
-    }
-    numberButtons.four = function() {
-        display.innerText += 4;
-        return firstArg += 4
-    }
-    numberButtons.five = function() {
-        display.innerText += 5;
-        return firstArg +=5
-    }
-    numberButtons.six = function() {
-        display.innerText += 6;
-        return firstArg += 6
-    }
-    numberButtons.seven = function() {
-        display.innerText += 7;
-        return firstArg +=7
-    }
-    numberButtons.eight = function() {
-        display.innerText += 8;
-        return firstArg+= 8
-    }
-    numberButtons.nine = function() {
-        display.innerText += 9;
-        return firstArg+= 9
-    }
-
-    numberButtons.arrayArguments = function() {
-        return result.push(parseInt(display.innerText))
-    }
-    return numberButtons;
-}();
 
 const operationPusher = function() {
     const push = {}
 
     push.addPush = function() {
-        secondArg = Number(firstArg)
-        firstArg = ''
-        display.innerText = '';
-        return (operations.add(0, result[0]));
+        result = operations.add(Number(firstArg), Number(secondArg));
+        secondArg = firstArg
+        firstArg = 0;
+        display.innerText += '+';
+        storedOperator = operations.add;
+        return result;
     }
     push.subtractPush = function() {
-        secondArg = Number(firstArg)
-        firstArg = ''
-        display.innerText = ''
-        return result.push(operations.subtract(0, result[0]));
+        result = operations.subtract(Number(firstArg), Number(secondArg));
+        secondArg = firstArg
+        firstArg = 0;
+        display.innerText += '-';
+        storedOperator = operations.subtract;
+        return result;
     }
     push.multiplyPush = function() {
-        secondArg = Number(firstArg)
-        firstArg = ''
-        display.innerText += 'x'
-        return result.push(operations.multiply(0, result[0]));
+        result = operations.multiply(Number(firstArg), Number(secondArg));
+        secondArg = firstArg
+        firstArg = 0;
+        display.innerText += 'x';
+        storedOperator = operations.multiply;
+        return result;
     }
     push.dividePush = function() {
-        secondArg = Number(firstArg)
-        firstArg = ''
-        display.innerText += '÷'
-        return result.push(operations.divide(0, result[0]));
+        result = operations.divide(Number(firstArg), Number(secondArg));
+        secondArg = firstArg
+        firstArg = 0;
+        display.innerText += '÷';
+        storedOperator = operations.divide;
+        return result;
     }; 
     return push;
 }();
 
+const operate = function() {
+    display.innerText = ''
+    result = storedOperator(Number(secondArg), Number(firstArg))
+    display.innerText = result;
+    secondArg = 0;
+    firstArg = result;
+    return result;
+    //return secondArg = result;
+}
 
+
+for (const number of numberBtns) {
+    number.addEventListener('click', function(e) {
+        display.innerText += number.innerText
+        return firstArg += number.innerText
+    })
+}
+
+//const argOneButton = document.querySelector('.operator-equals').addEventListener('click', argumentOne);
+const addButton = document.querySelector('.operator-add').addEventListener('click', operationPusher.addPush);
+const subtractButton = document.querySelector('.operator-subtract').addEventListener('click', operationPusher.subtractPush);
+const multiplyButton = document.querySelector('.operator-multiply').addEventListener('click', operationPusher.multiplyPush)
+const divideButton = document.querySelector('.operator-divide').addEventListener('click', operationPusher.dividePush);
+const equalsButton = document.querySelector('.operator-equals').addEventListener('click', operate);
+/*
 const oneButton = document.querySelector('.number-one').addEventListener('click', numbers.one);
 const twoButton = document.querySelector('.number-two').addEventListener('click', numbers.two);
 const threeButton = document.querySelector('.number-three').addEventListener('click', numbers.three);
@@ -118,19 +106,14 @@ const sixButton = document.querySelector('.number-six').addEventListener('click'
 const sevenButton = document.querySelector('.number-seven').addEventListener('click', numbers.seven);
 const eightButton = document.querySelector('.number-eight').addEventListener('click', numbers.eight);
 const nineButton = document.querySelector('.number-nine').addEventListener('click', numbers.nine);
-
-//const argOneButton = document.querySelector('.equals').addEventListener('click', numbers.argumentOne);
-const addButton = document.querySelector('.operator-add').addEventListener('click', operationPusher.addPush);
-const subtractButton = document.querySelector('.operator-subtract').addEventListener('click', operationPusher.subtractPush);
-const multiplyButton = document.querySelector('.operator-multiply').addEventListener('click', operationPusher.multiplyPush)
-const divideButton = document.querySelector('.operator-divide').addEventListener('click', operationPusher.dividePush);
-
-
-
-
-//const equalButton = document.querySelector('.equals').addEventListener('click', equals);
-/*document.addEventListener('click', ({ target }) => {
-    if (target.matches('button')) {
-      display.innerText = ''
+arrayArguments = function() {
+        return result.push(parseInt(display.innerText))
     }
-  }, {once: true});*/
+
+
+for (const operator of operatorBtns) {
+    operator.addEventListener('click', function(e) {
+        return operations.innerText()
+    })
+}*/
+
